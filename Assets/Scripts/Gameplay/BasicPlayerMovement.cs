03da3fa3477f9615207151a3a182516e02ff3ab9
Kudoshi <brendenguan@live.com>
2025-11-29 04:24:00 +0800

    feat: temporary jump and movement and camera

--- Assets/Scripts/Gameplay/BasicPlayerMovement.cs
+++ Assets/Scripts/Gameplay/BasicPlayerMovement.cs
@@ -4,46 +4,57 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 #endif
 
-/// <summary>
-/// A basic example of client authoritative movement. It works in both client-server and distributed-authority scenarios.
-/// If you want to modify this Script please copy it into your own project and add it to your Player Prefab.
-/// </summary>
 public class BasicPlayerMovement : MonoBehaviour
 {
-    public float Speed = 5;
-
+    public float Speed = 5f;
     public float Rotate = 150f;
+    public float JumpForce = 6f;
+
+    public float groundCheckDistance = 0.2f;
+    public LayerMask groundLayer;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true; // stop tipping over
+    }
 
     void Update()
     {
+        float rotateMultiplier = Rotate * Time.deltaTime;
 
-        var multiplier = Speed * Time.deltaTime;
-        var rotateMultiplier = Rotate * Time.deltaTime;
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+        }
+    }
 
-        // Old input backends are enabled.
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += new Vector3(-multiplier, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += new Vector3(multiplier, 0, 0);
-        }
+    void FixedUpdate()
+    {
+        float multiplier = Speed * Time.fixedDeltaTime;
+        Vector3 move = Vector3.zero;
+
+        // Movement based on player direction
         if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += new Vector3(0, 0, multiplier);
-        }
+            move += transform.forward;
+
         if (Input.GetKey(KeyCode.S))
-        {
-            transform.position += new Vector3(0, 0, -multiplier);
-        }
-        if (Input.GetKey(KeyCode.Z))
-        {
-            transform.Rotate(new Vector3(0, -rotateMultiplier));
-        }
-        if (Input.GetKey(KeyCode.X))
-        {
-            transform.Rotate(new Vector3(0, rotateMultiplier));
-        }
+            move -= transform.forward;
+
+        if (Input.GetKey(KeyCode.D))
+            move += transform.right;
+
+        if (Input.GetKey(KeyCode.A))
+            move -= transform.right;
+
+        rb.MovePosition(rb.position + move.normalized * Speed * Time.fixedDeltaTime);
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundLayer);
     }
 }
