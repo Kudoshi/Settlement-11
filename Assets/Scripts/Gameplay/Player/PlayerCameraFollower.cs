50964cf038ef0578b436253ffaeb178d8b4d5163
Kudoshi <brendenguan@live.com>
2025-11-29 16:57:46 +0800

    fix: player camera prefab

--- Assets/Scripts/Gameplay/Player/PlayerCameraFollower.cs
+++ Assets/Scripts/Gameplay/Player/PlayerCameraFollower.cs
@@ -1,8 +1,11 @@
 
+using Kudoshi.Utilities;
 using UnityEngine;
 
-public class PlayerCameraFollower : MonoBehaviour
+public class PlayerCameraFollower : Singleton<PlayerCameraFollower>
 {
+    public Transform HeadOrientation;
+    public Transform Head;
     private void Update()
     {
         transform.rotation = Camera.main.transform.rotation;
