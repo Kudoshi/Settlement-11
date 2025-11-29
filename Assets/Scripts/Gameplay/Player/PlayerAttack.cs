8ae8b35b25b09aea48559c4f44f3a653ef2f939f
BingChiau <118031847+bingchiau@users.noreply.github.com>
2025-11-29 11:35:53 +0800

    Add player attack combo

---
+++ Assets/Scripts/Gameplay/Player/PlayerAttack.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerAttack : MonoBehaviour
+{
+    List<string> attack_anim = new List<string> {"attack1", "attack2"};
+    public Animator animator;
+    public int comboCount;
+    public float reset;
+    public float resetTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1") && comboCount < 2)
+        {
+            Debug.Log("update");
+            animator.SetTrigger(attack_anim[comboCount]);
+            comboCount++;
+            reset = 0f;
+        }
+        if (comboCount > 0)
+        {
+            reset += Time.deltaTime;
+            if (reset > resetTime)
+            {
+                animator.SetTrigger("reset");
+                comboCount = 0;
+            }
+        }
+        if (comboCount == 2)
+        {
+            resetTime = 2f;
+            comboCount = 0;
+        }
+        else
+        {
+            resetTime = 1f;
+        }
+    }
+}
