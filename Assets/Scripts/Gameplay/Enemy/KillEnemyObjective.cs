640ee7b9dbd3df20967bd84504c32089f9c2fd5f
Kudoshi <brendenguan@live.com>
2025-12-01 22:46:30 +0800

    feat: enemy objective UI

--- Assets/Scripts/Gameplay/Enemy/KillEnemyObjective.cs
+++ Assets/Scripts/Gameplay/Enemy/KillEnemyObjective.cs
@@ -1,15 +1,23 @@
 using Kudoshi.Utilities;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class KillEnemyObjective : Singleton<KillEnemyObjective>
 {
+    [SerializeField] private TextMeshProUGUI _objText;
     [SerializeField] private int _maxEnemyKillAmountToWin;
     private int _enemyKillCount;
+
+    private void Start()
+    {
+        _objText.text = $"Kill {_enemyKillCount} / {_maxEnemyKillAmountToWin}";
+    }
 
     public void EnemyKilled()
     {
         _enemyKillCount++;
+        _objText.text = $"Kill {_enemyKillCount} / {_maxEnemyKillAmountToWin}";
 
         if (_enemyKillCount >= _maxEnemyKillAmountToWin)
         {
